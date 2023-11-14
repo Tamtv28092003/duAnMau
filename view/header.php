7ec4cb31e6e2e3806d2808485bc53b0c7f851007
tamtv <tamtvph43776@fpt.edu.vn>
2023-11-15 00:58:03 +0700

    done

--- view/header.php
+++ view/header.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Dự án mẫu</title>
-  <link rel="stylesheet" href="view/css/styles.css" />
-</head>
 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="stylesheet" href="../view/css/styles.css">
+    </head>
+   
 <body>
   <div class="boxcenter">
     <div class="row mb header">
@@ -24,3 +24,4 @@
         <li><a href="index.php?act=hoidap">Hỏi đáp</a></li>
       </ul>
     </div>
+    
